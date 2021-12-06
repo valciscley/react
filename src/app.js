b0decd1802d4f65c80ae5f4c183630ff5689deb3
Valciscley Ribeiro <valcis@gmail.com>
2021-12-06 16:16:55 -0300

    Incluindo arquivos do projeto

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import "app.css";
+
+import vflex from "./img/vflex.png";
+
+const App = () => (
+	<>
+		<h1>Virtuaflex</h1>
+		<img src={vflex} />
+	</>
+);
+
+export default App;
